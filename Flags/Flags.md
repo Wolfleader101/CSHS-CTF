e9f24d394f4e26c174d368e0666b5ea472f64e6b
Wolfleader <james.morgan@iinet.net.au>
2018-12-06 08:15:39 +0800

    Added Basic-1
    Enjoy ;)
    Flag submission will made later today for now submit it to discord
    :dart:

---
+++ Flags/Flags.md
@@ -0,0 +1,2 @@
+#WARNING FLAGS ARE IN HERE!!
+We strongly advise you don't cheat.
---
+++ Flags/flags.js
@@ -0,0 +1,7 @@
+const flags = {
+  "Flags": {
+    "Basic1": "CSHS-CTF{4dm1n_J03_decrypted}"
+  }
+}
+
+const basic1 = flags.Flags.Basic1;
